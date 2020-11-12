d81e72527982357516e3c4dd19adde8bf3517c60
PIERLEONI FABRIZIO <fabrizio.pierleoni@cnr.it>
2020-11-12 17:07:51 +0100

    inserito encoder per il submit di form

--- src/main/java/it/cnr/ict/config/OilConfiguration.java
+++ src/main/java/it/cnr/ict/config/OilConfiguration.java
@@ -2,11 +2,10 @@ package it.cnr.ict.config;
 
 import feign.Feign;
 import feign.auth.BasicAuthRequestInterceptor;
+import feign.form.FormEncoder;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
 import it.cnr.ict.repository.Oil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -17,17 +16,14 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(OilConfigurationProperties.class)
 public class OilConfiguration {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OilConfiguration.class);
-
     @Bean
-    public Oil oil(OilConfigurationProperties ocp) {
+    public Oil oil(OilConfigurationProperties ocp) { ;
         return Feign.builder()
                 .requestInterceptor(new BasicAuthRequestInterceptor(ocp.getUsername(), ocp.getPassword()))
-                .encoder(new GsonEncoder())
+                .encoder(new FormEncoder(new GsonEncoder()))
                 .decoder(new GsonDecoder())
-//                .logger(new Slf4jLogger(Oil.class))
-//                .logLevel(Logger.Level.FULL)
                 .target(Oil.class, ocp.getUrl());
+
     }
 
 }
